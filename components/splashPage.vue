06f3282d7c0a5de6a9d6dcb28a41c1c3396b57c7
Olawanle Joel <info4tobest2@gmail.com>
2021-04-16 17:46:25 +0100

    new splash page design

--- components/splashPage.vue
+++ components/splashPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="splash-bg">
-    <img src="~/assets/images/logo.svg" alt="" />
+    <img src="~/assets/images/logo.PNG" alt="" />
   </div>
 </template>
 
@@ -12,9 +12,14 @@ export default {}
 .splash-bg {
   height: 100vh;
   width: 100%;
-  background: url('~/assets/images/splash-page bg.jpg') no-repeat fixed center;
+  background-color: #0e448f;
+  background-image: url('https://www.transparenttextures.com/patterns/inspiration-geometry.png');
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.splash-bg IMG {
+  max-width: 100%;
 }
 </style>
