370b7ba2c67282ca147d6e13cb8ebbe36b48179f
Olawanle Joel <info4tobest2@gmail.com>
2021-04-20 12:28:00 +0100

    creating new pages

---
+++ pages/aboutPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bg">
+    <div class="back" @click="goBack">
+      <backIcon />
+    </div>
+    <div class="top">
+      <div class="info">
+        <h2>About Us</h2>
+        <p>We are on a mission!</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'bottomBarLayout',
+}
+</script>
+
+<style scoped>
+.bg {
+  padding: 10px;
+}
+.back {
+  margin: 40px 0 20px 14px;
+}
+.top .info {
+  margin: 50px 0 30px 14px;
+}
+
+.top .info h2 {
+  font-size: 20px;
+}
+
+.top .info p {
+  font-weight: bold;
+  color: #2165c6;
+  font-size: 15px;
+}
+</style>
